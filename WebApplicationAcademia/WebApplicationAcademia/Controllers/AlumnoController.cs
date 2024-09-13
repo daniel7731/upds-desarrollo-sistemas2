58b7b5842be8f51b3265deed1a83fd9910ef4baa
Daniel Roman <droman@gruposion.bo>
2024-09-13 09:48:31 -0400

    commit inicial

---
+++ WebApplicationAcademia/WebApplicationAcademia/Controllers/AlumnoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApplicationAcademia.Data;
+using WebApplicationAcademia.Model;
+
+namespace WebApplicationAcademia.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlumnoController : ControllerBase
+    {
+        private DaoAlumno daoAlumno = new DaoAlumno();
+        private readonly ILogger<WeatherForecastController> _logger;
+
+
+        [HttpGet("listarTodos")]
+        public List<Alummno> getLista()
+        {
+            return daoAlumno.getAll();
+        }
+        [HttpPost("registrar")]
+        public Alummno registrarAlumno(Alummno alummno)
+        {
+
+            if (alummno != null)
+            {
+                return daoAlumno.registrarAlumno(alummno);
+            }
+            return null;
+
+        }
+        [HttpPost("{id}/actualizar")]
+        public bool actualizarAlumno(int id, Alummno alummno)
+        {
+
+            return daoAlumno.actualizarAlummno(id, alummno);
+        }
+
+
+    }
+    
+}
---
+++ WebApplicationAcademia/WebApplicationAcademia/Model/Alummno.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplicationAcademia.Model
+{
+    public class Alummno
+    {
+        public string   nombre { get; set; }
+        public string   apellido1 { get; set; }
+        public string   apellido2 { get; set; }
+        public DateTime fechaModificacion { get; set;}
+        public DateTime fechaRegistro { get; set; }
+        public int      id { get;set;}
+        public DateTime fechaNacimiento { get;set; }
+    }
+}
